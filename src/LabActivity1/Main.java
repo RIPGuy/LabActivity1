867507077cd89f8516ce9ba1c520e9cd3601d959
Jasper Jimeno <Jasper Jimeno@DESKTOP-9SHG0F3>
2022-04-27 11:34:22 +0800

    hihihi

--- src/LabActivity1/Main.java
+++ src/LabActivity1/Main.java
@@ -1,12 +1,11 @@
 package LabActivity1;
 
 public class Main {
-	
-	public static void main (String [] args) {
-		Student s1 = new Student();
-		System.out.println(s1);
-		Teacher t1 = new Teacher();
-		System.out.println(t1);
-	}
-	
+
+    public static void main (String [] args) {
+    	Student s1 = new Student();
+    	
+        Teacher t1 = new Teacher("Ariane Pearl", "2020-0291", "Abunda");
+        System.out.println(t1);
+    }
 }
